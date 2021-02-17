a1195fa46160fcb94f84a98cb165be0fa4aa5a4f
Yanoni <onyan753@gmail.com>
2021-02-17 22:26:25 +0800

    used env var for PORT

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.post ('/imageurl', (req, res) => {
 })
 
 // Listen for requests
-app.listen (3000, () => {
-	console.log('App is running on port 3000');
+app.listen (process.env.PORT || 3000, () => {
+	console.log(`App is running on port ${process.env.PORT}`);
 })
